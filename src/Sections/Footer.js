9b68b7ae48a4dd1d3ca50d35bdcc7a87337c2c2b
Chris Elias <chris@Chriss-Mac-mini.local>
2019-09-02 19:37:10 -0400

    changed icon and added my brand logo

--- src/Sections/Footer.js
+++ src/Sections/Footer.js
@@ -3,30 +3,33 @@ import React from "react";
 const Footer = ({ github, resume, linkedin }) => {
   return (
     <footer class="footer">
-      {/* <div class="footer-container"> */}
-      <div class="menu-container">
-        <ul class="menu-list">
-          <li class="menu-list-item">
-            <a href={github} target="_blank" rel="noopener noreferrer">
-              GITHUB
-            </a>
-          </li>
-          <li class="menu-list-item">
-            <a href={linkedin} target="_blank" rel="noopener noreferrer">
-              LINKEDIN
-            </a>
-          </li>
-          <li class="menu-list-item">
-            <a href={resume} target="_blank" rel="noopener noreferrer">
-              RESUME
-            </a>
-          </li>
-        </ul>
+      <div class="footer-logo-box">
+        <img src="images/logo.png" alt="Chris" class="footer-logo" />
       </div>
-      <div class="text-container">
-        <p class="footer-text">Copyright &copy; 2019</p>
+      <div class="footer-row">
+        <div class="menu-container">
+          <ul class="menu-list">
+            <li class="menu-list-item">
+              <a href={github} target="_blank" rel="noopener noreferrer">
+                GITHUB
+              </a>
+            </li>
+            <li class="menu-list-item">
+              <a href={linkedin} target="_blank" rel="noopener noreferrer">
+                LINKEDIN
+              </a>
+            </li>
+            <li class="menu-list-item">
+              <a href={resume} target="_blank" rel="noopener noreferrer">
+                RESUME
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="text-container">
+          <p class="footer-text">Copyright &copy; 2019</p>
+        </div>
       </div>
-      {/* </div> */}
     </footer>
   );
 };
--- src/Sections/Header.js
+++ src/Sections/Header.js
@@ -3,11 +3,10 @@ import React from "react";
 const Header = ({ name }) => {
   return (
     <header class="header">
+      <div class="header-logo-box">
+        <img src="images/logo.png" alt="Chris" class="header-logo" />
+      </div>
       <div class="header__text-box">
-        {/* <svg viewBox="0 0 16 16" class="icon icon--center icon--logo icon-logo">
-	<use xlink:href="#icon-logo"></use>
-</svg> */}
-
         <h1 class="heading-primary">
           <span className="heading-primary-main">{name} </span>
           <span className="heading-primary-sub">Front End Developer</span>

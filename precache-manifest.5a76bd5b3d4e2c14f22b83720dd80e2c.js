80eef307742782b17416d8389dd4196dcf03dbd2
Chris Elias <chris@Chriss-Mac-mini.local>
2019-09-01 23:40:22 -0400

    Updates

---
+++ precache-manifest.5a76bd5b3d4e2c14f22b83720dd80e2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce505f2c637642d27fc8242f9b841758",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4ac50b984e25b11915a3",
+    "url": "/portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "39b2155d063acf683d59",
+    "url": "/portfolio/static/js/2.889a8c23.chunk.js"
+  },
+  {
+    "revision": "4ac50b984e25b11915a3",
+    "url": "/portfolio/static/js/main.3614e646.chunk.js"
+  },
+  {
+    "revision": "3a158f54790be365c345",
+    "url": "/portfolio/static/js/runtime~main.d7872d27.js"
+  }
+]);

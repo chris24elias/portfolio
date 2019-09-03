294200eb408a533621bd8422a1ad1bbedc544c4a
Chris Elias <chris@Chriss-Mac-mini.local>
2019-09-02 20:27:54 -0400

    Updates

---
+++ precache-manifest.328253e5d5d0e0afafcef653bf07f75f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d0a032924b5363d5ccad722217ea0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a01788a9f1df3e97401e",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "39b2155d063acf683d59",
+    "url": "/static/js/2.889a8c23.chunk.js"
+  },
+  {
+    "revision": "a01788a9f1df3e97401e",
+    "url": "/static/js/main.983ce650.chunk.js"
+  },
+  {
+    "revision": "60b1462a467b934acd36",
+    "url": "/static/js/runtime~main.8e283c92.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "thechriselias.com/precache-manifest.3ee458504608f9c9617ebf4a8019c999.js"
+  "/precache-manifest.328253e5d5d0e0afafcef653bf07f75f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("thechriselias.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
